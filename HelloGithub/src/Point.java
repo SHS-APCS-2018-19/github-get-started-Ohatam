302b66eb29f3cf60046823937859d2ffac436e98
Ohatam <45826847+Ohatam@users.noreply.github.com>
2019-01-09 09:30:26 -0800

    stuff happened
    yup

--- HelloGithub/src/Point.java
+++ HelloGithub/src/Point.java
@@ -1,12 +1,34 @@
 
 public class Point {
-	
 	double x;
 	double y;
+	
 	public Point (int x1, int y1) {
-		x=x1;
-		y=y1;
-	System.out.print(x+","+y);
+		setX(x1);
+		setY(y1);
 	}
+	public double getX() {
+		return x;
+	}
+	public double gety() {
+		return y;
+	}
+	public void setX(double x) {
+		this.x= x;
+		
+	}
+	public void setY(double y) {
+		this.y= y;
+	}
+	
+	public double distanceTo(Point point2) {
+		double distance = Math.sqrt(Math.pow((point2.x-this.x), 2)+(Math.pow((point2.y-this.y),2)));
+		return distance;
+	}
+	public void polar() {
+		double radius = Math.sqrt(Math.pow(x, 2)+(Math.pow(y,2)));
+		double theta=Math.atan2(y,x);
+		System.out.print("Length is: " + radius + "\n" + "Angle is: " + theta);
+		}
 	
 }
--- HelloGithub/src/PointMain.java
+++ HelloGithub/src/PointMain.java
@@ -6,6 +6,8 @@ public class PointMain {
 		Scanner inpt = new Scanner(System.in);
 		Point point1 = new Point(inpt.nextInt(),inpt.nextInt());
 		Point point2 = new Point(inpt.nextInt(),inpt.nextInt());
+		System.out.println(point1.distanceTo(point2));
+		point1.polar();
 	}
 
 }
